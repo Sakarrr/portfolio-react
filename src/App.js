d4fc185a916f22254582488051ced9d5f101f689
Sakarr <ctha.sakar@gmail.com>
2024-02-22 22:03:26 +0545

    Update - Folder/file structure and set-up dummy styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from "./logo.svg";
-import Home from "./pages/home";
+import "./assets/styles/style.scss";
+import Home from "./components/Home";
 import "./App.css";
 
 function App() {
